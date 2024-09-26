a000662fa5fdaa70d91b112a143890e19f899f89
LijaSoni <lijasalim@gmail.com>
2024-09-26 11:45:58 +0530

    Add project files.

---
+++ MailSendAPPln/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace MailSendAPPln
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SendEmail2("Lija", "lijasalim@gmail.com", "jqcz negv afpl cstu", "Appu", "lijasalim@gmail.com", "Haiii", "test");
+        }
+        public static void SendEmail2(string yourName, string yourGmailUserName, string yourGmailPassword, string toName, string toEmail, string subject, string body)
+
+        {
+            string to = toEmail; //To address    
+            string from = yourGmailUserName; //From address    
+            MailMessage message = new MailMessage(from, to);
+
+            string mailbody =body;
+            message.Subject =subject;
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+            System.Net.NetworkCredential basicCredential1 = new
+            System.Net.NetworkCredential(yourGmailUserName,yourGmailPassword);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = true;
+            client.Credentials = basicCredential1;
+            try
+            {
+                client.Send(message);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+    }
+}
